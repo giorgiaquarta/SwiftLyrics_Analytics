378db4381c4cb77749e5064b244a429a4e72e9a3
giorgia.quarta3@studio.unibo.it <giorgyqua@gmail.com>
2025-10-24 12:52:43 +0200

    [INST] installed dependencies and edited pyproject file

---
+++ build/lib/SwiftLyrics_Analytics/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from .__version__ import __version__
+
+__author__ = ['Giorgia Quarta']
+__email__ = ['giorgia.quarta3@studio.unibo.it']
+__all__ = [
+    '__version__'
+]
---
+++ build/lib/SwiftLyrics_Analytics/__version__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = ['Giorgia Quarta']
+__email__ = ['giorgia.quarta3@studio.unibo.it']
+
+__version__ = '0.0.1'

c9c6d60304cc23ea2107c32b11153771992ed5df
giorgia.quarta3@studio.unibo.it <giorgyqua@gmail.com>
2025-11-18 17:28:35 +0100

    [PY] edited main module

---
+++ SwiftLyrics_Analytics/SwiftLyrics_Analytics.py
@@ -0,0 +1,51 @@
+import dataset
+import format
+import wc
+import nn_analysis
+import sys
+
+usage = """
+USAGE: SwiftLyrics_Analytics.py COMMAND
+
+COMMAND may be one of the following:
+    - dataset LYRICS_FILENAME ALBUM_FILENAME: print the heading lines of the dataset
+    - format LYRICS_FILENAME: clean the lyrics and print the data frame
+    - wordcloud LYRICS_FILENAME ALBUM_FILENAME: display the wordcloud of the dataset
+    - analysis LYRICS_FILENAME ALBUM_FILENAME: perform a neural network analysis of the dataset
+"""
+
+
+def dataset_command(lyrics, albums):
+    lyrics = dataset.parse_lyrics(lyrics)
+    albums = dataset.parse_albums(albums)
+    print(lyrics.head())
+    print(albums.head())
+
+
+def format_command(lyrics):
+    print("pippo")
+
+
+def wordcloud_command(lyrics, albums):
+    print("pippo")
+
+
+def analysis_command(lyrics, albums):
+    print("pippo")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(usage)
+
+    match sys.argv[1]:
+        case "dataset":
+            dataset_command(sys.argv[2], sys.argv[3])
+        case "format":
+            format_command(sys.argv[2])
+        case "wordcloud":
+            wordcloud_command(sys.argv[2], sys.argv[3])
+        case "analysis":
+            analysis_command(sys.argv[2], sys.argv[3])
+        case _:
+            sys.exit(usage)

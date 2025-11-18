192db9ff3471e059ad5b835dd2602065c37caaab
giorgia.quarta3@studio.unibo.it <giorgyqua@gmail.com>
2025-11-18 16:57:51 +0100

    [PY] edited dataset file

--- SwiftLyrics_Analytics/dataset.py
+++ SwiftLyrics_Analytics/dataset.py
@@ -1,29 +1,58 @@
 import pandas as pd
 
-def parse_df(filename: str, headers: list[str], delimiter: str = ';') -> pd.DataFrame :
 
+def parse_df(
+    filename: str, headers: list[str] = None, header=None, delimiter: str = ";"
+) -> pd.DataFrame:
     raw = None
-    try: 
+    try:
         raw = pd.read_csv(
-            f'dataset/{filename}',
-            delimiter=':',
-            header=None,
-            names=headers
+            f"../dataset/{filename}", delimiter=delimiter, header=header, names=headers
         )
     except Exception as exc:
         print(exc)
 
     return raw
 
+
 def parse_lyrics(filename: str) -> pd.DataFrame:
-    raw_lyrics = parse_df(filename, ['album_id', 'track_id', 'line_n', 'part', 'lyric'])
-    lyrics = raw.lyrics.groupby(['album_id','track_id'])['lyric'].apply(lambda lines: "\n".join(lines)).reset_index()
+    raw_lyrics = parse_df(
+        filename, ["album_id", "track_id", "line_n", "part", "lyric"], delimiter=":"
+    )
+    lyrics = (
+        raw_lyrics.groupby(["album_id", "track_id"])["lyric"]
+        .apply(lambda lines: "\n".join(lines))
+        .reset_index()
+    )
 
     return lyrics
 
-def parse_albums(filename: str) -> pd.DataFrame:
-    raw.albums = parse_df(filename, [''])
 
-if __name__ = "__main__"
+def parse_albums(filename: str) -> pd.DataFrame:
+    raw_albums = parse_df(
+        filename,
+        header="infer",
+    )
+    albums = raw_albums.drop(
+        [
+            "SubTitle",
+            "LowestFqWord",
+            "PrevalentVerb",
+            "PrevalentAdjective",
+            "PrevalentNoun",
+            "Songs",
+            "Lines",
+            "Words",
+        ],
+        axis=1,
+    )
+
+    return albums
+
+
+if __name__ == "__main__":
     lyrics = parse_lyrics("lyrics.csv")
     print(lyrics.head(20))
+
+    albums = parse_albums("albums.csv")
+    print(albums.head(13))
